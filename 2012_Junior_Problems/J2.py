276f96083754bfdbed9b6aceb44224c1e7138cf6
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:34:23 -0400

    Update J2.py

--- 2012_Junior_Problems/J2.py
+++ 2012_Junior_Problems/J2.py
@@ -1,20 +1,23 @@
 # CCC 2012 Problem J2: Sounds fishy!
 # Programmer: Foram Gandhi
 
+def sounds_fishy():
+    
+    reading1= int(input())
+    reading2= int(input())
+    reading3= int(input())
+    reading4= int(input())
 
-reading1= int(input())
-reading2= int(input())
-reading3= int(input())
-reading4= int(input())
+    if (reading1 < reading2 and reading2 < reading3 and reading3 < reading4):
+        print("Fish Rising")
+    
+    elif (reading2 < reading1 and reading3 < reading2 and reading4 < reading3):
+        print("Fish Diving")
+    
+    elif (reading1 == reading2 and reading2 == reading3 and reading3 == reading4):
+        print("Fish at Constant Depth")
+    
+    else:
+        print("No Fish")
 
-if (reading1 < reading2 and reading2 < reading3 and reading3 < reading4):
-    print("Fish Rising")
-    
-elif (reading2 < reading1 and reading3 < reading2 and reading4 < reading3):
-    print("Fish Diving")
-    
-elif (reading1 == reading2 and reading2 == reading3 and reading3 == reading4):
-    print("Fish at Constant Depth")
-    
-else:
-    print("No Fish")
+sounds_fishy()
