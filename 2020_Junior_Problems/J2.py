3775cbd14fe96e73ea7bd93315af341223168c54
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 18:58:37 -0400

    Update J2.py

--- 2020_Junior_Problems/J2.py
+++ 2020_Junior_Problems/J2.py
@@ -1,23 +1,25 @@
-# CCC 2020 Problem J2: Epidemiolgy
+# CCC 2020 Problem J2: Epidemiology
 # Programmer: Foram Gandhi
 
+def epidemiology()
+    p = int(input())
+    n = int(input())
+    r = int(input())
 
-p = int(input())
-n = int(input())
-r = int(input())
+    infected = n
+    people = n
 
-infected = n
-people = n
+    count = 0
+    while True:
+        people = people + (infected * r)
+        infected = infected * r
+        count += 1
 
-count = 0
-while True:
-    people = people + (infected * r)
-    infected = infected * r
-    count += 1
-
-    if people >= p:
-        if people == p:
-            print(count + 1)
-        else:
-            print(count)
-        break
+        if people >= p:
+            if people == p:
+                print(count + 1)
+            else:
+                print(count)
+            break
+            
+epidemiology()
