0d28d769306f98ba3ed0cae3606e4ce3ac00db7b
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 18:20:13 -0400

    Update J5.py

--- 2016_Junior_Problems/J5.py
+++ 2016_Junior_Problems/J5.py
@@ -1,30 +1,33 @@
-# CCC 2016 Problem J4: Arrival Time
+# CCC 2016 Problem J5: Tandem Bicycle
 # Programmer: Foram Gandhi
 
-question = int(input())
-pairs = int(input())
+def tandem_bicycle():
+    question = int(input())
+    pairs = int(input())
 
-d_speed = input().split(" ")
-p_speed = input().split(" ")
+    d_speed = input().split(" ")
+    p_speed = input().split(" ")
 
-dmojistan_speed = [int(x) for s in d_speed]
-pegland_speed = [int(x) for s in p_speed]
+    dmojistan_speed = [int(x) for s in d_speed]
+    pegland_speed = [int(x) for s in p_speed]
 
-if question == 1:
-    dmojistan_speed.sort()
-    pegland_speed.sort()
+    if question == 1:
+        dmojistan_speed.sort()
+        pegland_speed.sort()
     
-    total_speed = 0
-    for t in range(pairs):
-        total_speed += max(dmojistan_speed[t], pegland_speed[t])
-    print(total_speed)
+        total_speed = 0
+        for t in range(pairs):
+            total_speed += max(dmojistan_speed[t], pegland_speed[t])
+        print(total_speed)
 
 
-elif question == 2:
-    dmojistan_speed.sort()
-    pegland_speed.sort(reverse = True)
+    elif question == 2:
+        dmojistan_speed.sort()
+        pegland_speed.sort(reverse = True)
     
-    total_speed = 0
-    for t in range(pairs):
-        total_speed += max(dmojistan_speed[t], pegland_speed[t])
-    print(total_speed)
+        total_speed = 0
+        for t in range(pairs):
+            total_speed += max(dmojistan_speed[t], pegland_speed[t])
+        print(total_speed)
+        
+tandem_bicycle()
