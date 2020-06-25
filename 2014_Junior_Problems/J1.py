964d50c5d4ccbb5bfc0a27d3fe0894cf5e3e67c9
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 16:56:50 -0400

    Create J1.py

---
+++ 2014_Junior_Problems/J1.py
@@ -0,0 +1,24 @@
+# CCC 2014 Problem J1: Triangle Times
+# Programmer: Foram Gandhi
+
+
+def triangle_times():
+  int1 = int(input())
+  int2 = int(input())
+  int3 = int(input())
+
+  sum_int = int1 + int2 + int3
+
+  if int1 == 60 and int2 == 60 and int3 == 60:
+      print("Equilateral")
+    
+  elif sum_int == 180 and int1 == int2 or int1 == int3 or int2 == int3:
+      print("Scalene")
+
+  elif sum_int != 180:
+      print("Error")
+
+  else:
+      print("Scalene")
+      
+triangle_times()
