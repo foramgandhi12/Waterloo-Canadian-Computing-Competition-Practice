f7c0014d74e5b217fbfcdfe0f365e4875f42d37b
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:06:08 -0400

    Create J1.py

---
+++ 2018_Junior_Problems/J1.py
@@ -0,0 +1,17 @@
+# CCC 2018 Problem J1: Telemarketer or not?
+# Programmer: Foram Gandhi
+
+
+def telemarketer():
+    lst = []
+
+    for i in range(4):
+        num = int(input())
+        lst.append(num)
+
+    if (lst[0] == 8 or lst[0] == 9) and (lst[3] == 8 or lst[3] == 9) and (lst[1] == lst[2]):
+        print("ignore")
+    else:
+        print("answer")
+
+telemarketer()
