1dd37433f5e7d3c1ddcf61d5d977cd5697925f3c
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:17:42 -0400

    Update J2.py

--- 2011_Junior_Problems/J2.py
+++ 2011_Junior_Problems/J2.py
@@ -1,18 +1,22 @@
 # CCC 2011 Problem J2: Who Has Seen The Wind
 # Programmer: Foram Gandhi
 
-h = int(input())
-max_hours = int(input())
 
-t = 1
-altitude = (-6*t*t*t*t) + (h*t*t*t) + (2*t*t) + t
+def wind():
+    h = int(input())
+    max_hours = int(input())
 
-while altitude > 0 and t < max_hours:
-    t += 1
+    t = 1
     altitude = (-6*t*t*t*t) + (h*t*t*t) + (2*t*t) + t
-    
-if altitude <= 0:
-    print("The balloon first touches ground at hour:\n", t)
 
-else:
-    print("The balloon does not touch ground in the given time.")
+    while altitude > 0 and t < max_hours:
+        t += 1
+        altitude = (-6*t*t*t*t) + (h*t*t*t) + (2*t*t) + t
+    
+    if altitude <= 0:
+        print("The balloon first touches ground at hour:\n", t)
+
+    else:
+        print("The balloon does not touch ground in the given time.")
+
+wind()
