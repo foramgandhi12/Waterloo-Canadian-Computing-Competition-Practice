c9987be60795be2e8acdc012a00d3155b8943408
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:42:30 -0400

    Update J4.py

--- 2014_Junior_Problems/J4.py
+++ 2014_Junior_Problems/J4.py
@@ -1,30 +1,32 @@
 # CCC 2014 Problem J4: Party Invitation
 # Programmer: Foram Gandhi
 
+def party_invitation():
+    k = int(input())
+    num_rounds = int(input())
 
-k = int(input())
-num_rounds = int(input())
+    l = []
 
-l = []
+    for m in range(num_rounds):
+        l.append(int(input())) # l is [2, 3]
 
-for m in range(num_rounds):
-    l.append(int(input())) # l is [2, 3]
+    list_invitees = []
+    invite = 1
 
-list_invitees = []
-invite = 1
+    while invite < k + 1:
+        list_invitees.append(invite)
+        invite += 1 # list_invitees is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-while invite < k + 1:
-    list_invitees.append(invite)
-    invite += 1 # list_invitees is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-for r in range(len(l)): # for each round
-    new_l = []
-    for i in range(len(list_invitees)): # each invitee
+    for r in range(len(l)): # for each round
+        new_l = []
+        for i in range(len(list_invitees)): # each invitee
         
-        if (i + 1) % l[r] == 0: # checks if ith position is divisible by round number
-            new_l.append(list_invitees[i]) # creates a list of what to remove
-    for j in range(len(new_l)): # each item in new list
-        list_invitees.remove(new_l[j]) # removes items in new list from original list of invitees
+            if (i + 1) % l[r] == 0: # checks if ith position is divisible by round number
+                new_l.append(list_invitees[i]) # creates a list of what to remove
+        for j in range(len(new_l)): # each item in new list
+            list_invitees.remove(new_l[j]) # removes items in new list from original list of invitees
     
-for k in list_invitees:
-    print(k)
+    for k in list_invitees:
+        print(k)
+        
+party_invitation()
