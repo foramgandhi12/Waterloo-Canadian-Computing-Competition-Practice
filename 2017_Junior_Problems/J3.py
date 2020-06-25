deeb35c61a5e83d48f3d5bbf26e58778d7d10152
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 18:27:29 -0400

    Update J3.py

--- 2017_Junior_Problems/J3.py
+++ 2017_Junior_Problems/J3.py
@@ -1,23 +1,25 @@
 # CCC 2017 Problem J3: Exactly Electrical
 # Programmer: Foram Gandhi
 
+def exactly_electrical():
+    start_coord = input().split()
+    x1 = int(start_coord[0])
+    y1 = int(start_coord[1])
 
-start_coord = input().split()
-x1 = int(start_coord[0])
-y1 = int(start_coord[1])
+    dest_coord = input().split()
+    x2 = int(dest_coord[0])
+    y2 = int(dest_coord[1])
 
-dest_coord = input().split()
-x2 = int(dest_coord[0])
-y2 = int(dest_coord[1])
+    battery_charge = int(input())
 
-battery_charge = int(input())
+    shortest_path = abs(x1 - x2) + abs(y1 - y2)
 
-shortest_path = abs(x1 - x2) + abs(y1 - y2)
-
-if battery_charge >= shortest_path:
-    if (battery_charge - shortest_path) % 2 == 0:
-        print("Y")
+    if battery_charge >= shortest_path:
+        if (battery_charge - shortest_path) % 2 == 0:
+            print("Y")
+        else:
+            print("N")
     else:
         print("N")
-else:
-    print("N")
+        
+exactly_electrical()
