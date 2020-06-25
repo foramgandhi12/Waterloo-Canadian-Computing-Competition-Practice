a6f1e86084349056ab51d2909b9723592e1ea29c
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 16:50:48 -0400

    Update J1.py

--- 2013_Junior_Problems/J1.py
+++ 2013_Junior_Problems/J1.py
@@ -1,1 +1,16 @@
+# CCC 2013 Problem J1: Next in line
+# Programmer: Foram Gandhi
 
+
+def next_in_line():
+  youngest = int(input())
+  middle = int(input())
+
+  if 0 <= youngest <= 50 and 0 <= middle <= 50:
+      oldest = middle + (middle - youngest)
+      print(oldest)
+
+  else:
+      print("Try again.")
+
+next_in_line()
