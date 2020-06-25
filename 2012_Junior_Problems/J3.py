9c0bff00ebbd8f7e237d5fc3f8323ca99e52e025
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:35:22 -0400

    Update J3.py

--- 2012_Junior_Problems/J3.py
+++ 2012_Junior_Problems/J3.py
@@ -1,20 +1,22 @@
 # CCC 2012 Problem J3: Icon Scaling
 # Programmer: Foram Gandhi
 
+def icon_scaling():
+    factor = int(input())
 
-factor = int(input())
+    count = 0
+    for count in range(factor):
+        print(("*" * factor) + ("X" * factor) + ("*" * factor))
+        count += 1
 
-count = 0
-for count in range(factor):
-    print(("*" * factor) + ("X" * factor) + ("*" * factor))
-    count += 1
+    i = 0
+    for i in range(factor):
+        print((" " * factor) + ("X" * (factor * 2)))
+        i += 1
 
-i = 0
-for i in range(factor):
-    print((" " * factor) + ("X" * (factor * 2)))
-    i += 1
-
-j = 0
-for j in range(factor):
-    print(("*" * factor) + (" " * factor) + ("*" * factor))
-    j += 1
+    j = 0
+    for j in range(factor):
+        print(("*" * factor) + (" " * factor) + ("*" * factor))
+        j += 1
+        
+icon_scaling()
