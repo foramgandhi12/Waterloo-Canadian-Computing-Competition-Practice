e8e953dac35824d4efdfb84c94d34d7245187a47
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:40:16 -0400

    Update J2.py

--- 2014_Junior_Problems/J2.py
+++ 2014_Junior_Problems/J2.py
@@ -1,24 +1,26 @@
 # CCC 2014 Problem J2: Vote Count
 # Programmer: Foram Gandhi
 
+def vote_count():
+    votes = int(input())
+    singer= input()
 
-votes = int(input())
-singer= input()
+    a_count = 0
+    b_count = 0
 
-a_count = 0
-b_count = 0
+    for letter in singer:
+        if letter == 'A':
+            a_count += 1
+        elif letter == 'B':
+            b_count += 1
 
-for letter in singer:
-    if letter == 'A':
-        a_count += 1
-    elif letter == 'B':
-        b_count += 1
+    if a_count > b_count:
+        print("A")
 
-if a_count > b_count:
-    print("A")
+    elif b_count > a_count:
+        print("B")
 
-elif b_count > a_count:
-    print("B")
-
-else:
-    print("Tie")
+    else:
+        print("Tie")
+        
+vote_count()
