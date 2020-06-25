5e637a6a6d3eed48fd6cb67a549464f0b89650bc
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 18:53:01 -0400

    Update J2.py

--- 2019_Junior_Problems/J2.py
+++ 2019_Junior_Problems/J2.py
@@ -1,15 +1,17 @@
 # CCC 2019 Problem J2: Time to Decompress
 # Programmer: Foram Gandhi
 
+def time_to_decompress():
+    l = int(input())
+    count = 0
+    lst = []
 
-l = int(input())
-count = 0
-lst = []
+    while count < l:
+        line = input().split(" ")
+        lst.append(line)
+        count += 1
 
-while count < l:
-    line = input().split(" ")
-    lst.append(line)
-    count += 1
-
-for i in range(len(lst)):
-    print(lst[i][1] * int(lst[i][0]))
+    for i in range(len(lst)):
+        print(lst[i][1] * int(lst[i][0]))
+        
+time_to_decompress()
