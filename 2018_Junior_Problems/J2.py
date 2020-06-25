66df19bd2e88eb294ccd969f516b8074b15a4a2a
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 18:49:32 -0400

    Update J2.py

--- 2018_Junior_Problems/J2.py
+++ 2018_Junior_Problems/J2.py
@@ -1,14 +1,16 @@
 # CCC 2018 Problem J2: Occupy Parking
 # Programmer: Foram Gandhi
 
+def occupy_parking():
+    n = int(input())
+    yesterday = input()
+    today = input()
 
-n = int(input())
-yesterday = input()
-today = input()
+    tf = 0
+    for i in range(len(yesterday)):
+        if yesterday[i] == "C" and today[i] == "C":
+            tf += 1
 
-tf = 0
-for i in range(len(yesterday)):
-    if yesterday[i] == "C" and today[i] == "C":
-        tf += 1
-
-print(tf)
+    print(tf)
+    
+occupy_parking()
