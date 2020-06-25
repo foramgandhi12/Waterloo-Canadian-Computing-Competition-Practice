38e3377a12eac9964cc4e52a8c6e1b1832bb547b
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:41:01 -0400

    Update J3.py

--- 2014_Junior_Problems/J3.py
+++ 2014_Junior_Problems/J3.py
@@ -1,27 +1,27 @@
 # CCC 2014 Problem J3: Double Dice
 # Programmer: Foram Gandhi
 
+def double_dice():
+    antonia = 100
+    david = 100
+    rounds = int(input())
 
-antonia = 100
-david = 100
-rounds = int(input())
+    count = 0
 
-count = 0
+    while count < rounds:
+        rolls = input().replace(" ", "")
+        a_points = int(rolls[0])
+        d_points = int(rolls[1])
 
-while count < rounds:
-    rolls = input().replace(" ", "")
-    a_points = int(rolls[0])
-    d_points = int(rolls[1])
-
-    if d_points < a_points:
-        david -= a_points
+        if d_points < a_points:
+            david -= a_points
         
-    elif a_points < d_points:
-        antonia -= d_points
+        elif a_points < d_points:
+            antonia -= d_points
         
-    count += 1
+        count += 1
 
-print(antonia)
-print(david)
-        
-    
+    print(antonia)
+    print(david)
+            
+double_dice()
