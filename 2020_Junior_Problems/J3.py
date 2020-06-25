71c6fc764b5126ac17dcf8474b3399f4e13491d7
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 19:01:48 -0400

    Update J3.py

--- 2020_Junior_Problems/J3.py
+++ 2020_Junior_Problems/J3.py
@@ -1,14 +1,16 @@
 # CCC 2020 Problem J3: Art
 # Programmer: Foram Gandhi
 
+def art():
+    n = int(input())
+    x, y = [], []
 
-n = int(input())
-x, y = [], []
-
-for i in range(n):
-    xs, ys = input().split(',')
-    x.append(int(xs))
-    y.append(int(ys))
+    for i in range(n):
+        xs, ys = input().split(',')
+        x.append(int(xs))
+        y.append(int(ys))
     
-print(f"{min(x) - 1}, {min(y) - 1}")
-print(f"{max(x) + 1}, {max(y) + 1}")
+    print(f"{min(x) - 1}, {min(y) - 1}")
+    print(f"{max(x) + 1}, {max(y) + 1}")
+    
+art()
