e930df950e34d6ae7947a0be024249ff376da17c
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:03:32 -0400

    Create J1.py

---
+++ 2016_Junior_Problems/J1.py
@@ -0,0 +1,27 @@
+# CCC 2016 Problem J1: Tournament Selection
+# Programmer: Foram Gandhi
+
+
+def tournament_selection():
+  games = 0
+  outcome = []
+
+  while games < 6:
+      win_loss = input()
+      outcome.append(win_loss)
+      games += 1
+
+  w = 0
+
+  for wl in range(len(outcome)):
+      if outcome[wl] == "W":
+          w += 1
+
+  if w == 5 or w == 6:
+      print("1")
+  elif w == 3 or w == 4:
+      print("2")
+  elif w == 1 or w == 2:
+      print("3")
+  else:
+      print("-1")
