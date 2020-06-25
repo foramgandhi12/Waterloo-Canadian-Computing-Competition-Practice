ba444fc57c669350c2c221e8501e26c3c42d74b0
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 19:03:00 -0400

    Update J4.py

--- 2020_Junior_Problems/J4.py
+++ 2020_Junior_Problems/J4.py
@@ -1,16 +1,18 @@
 # CCC 2020 Problem J4: Cyclic Shifts
 # Programmer: Foram Gandhi
 
+def cyclic_shifts():
+    t = input()
+    s = input()
+    lst = []
+    tf = "no"
 
-t = input()
-s = input()
-lst = []
-tf = "no"
-
-for i in range(len(s)):
-    if s in t:
-        tf = "yes"
-        break
-    s = s[-1] + s[:-1]
+    for i in range(len(s)):
+        if s in t:
+            tf = "yes"
+            break
+        s = s[-1] + s[:-1]
     
-print(tf)
+    print(tf)
+    
+cyclic_shifts()
