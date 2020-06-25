81aeb94448f8e40fc7a525bf903214ec7d8be684
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:47:35 -0400

    Update J3.py

--- 2015_Junior_Problems/J3.py
+++ 2015_Junior_Problems/J3.py
@@ -1,20 +1,22 @@
 # CCC 2015 Problem J3: Rovarspraket
 # Programmer: Foram Gandhi
 
+def rovarspraket():
+    word = input("")
+    wordLow = word.lower()
 
-word = input("")
-wordLow = word.lower()
+    closestVowel = "aaeeeiiiiooooouuuuuuu"                                                
+    consonant = "bcdfghjklmnpqrstvwxyz"                                                  
+    nextConsonant = "cdfghjklmnpqrstvwxyzz"                                          
+    newWord = ""                                                                      
 
-closestVowel = "aaeeeiiiiooooouuuuuuu"                                                
-consonant = "bcdfghjklmnpqrstvwxyz"                                                  
-nextConsonant = "cdfghjklmnpqrstvwxyzz"                                          
-newWord = ""                                                                      
-
-for i in range(len(wordLow)):
-    j = consonant.find(wordLow[i])    #stores the index of the first occurrence of i in the word entered by the user
-    newWord = newWord + wordLow[i]    #stores the original letter 
+    for i in range(len(wordLow)):
+        j = consonant.find(wordLow[i])    #stores the index of the first occurrence of i in the word entered by the user
+        newWord = newWord + wordLow[i]    #stores the original letter 
     
-    if j > -1:
-        newWord = newWord + closestVowel[j] + nextConsonant[j]    #adds the closest vowel and next consonant in terms of j
+        if j > -1:
+            newWord = newWord + closestVowel[j] + nextConsonant[j]    #adds the closest vowel and next consonant in terms of j
         
-print(newWord)
+    print(newWord)
+    
+rovarspraket()
