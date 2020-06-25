d620089ff474cd712cf5bd0cd897f6933dcb4e2f
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 18:17:14 -0400

    Update J3.py

--- 2016_Junior_Problems/J3.py
+++ 2016_Junior_Problems/J3.py
@@ -1,15 +1,17 @@
 # CCC 2016 Problem J3: Hidden Palindrome
 # Programmer: Foram Gandhi
 
+def hidden_palindrome():
+    word = input()
+    l = []
 
-word = input()
-l = []
+    for i in range(len(word)):
+        for j in range(i, len(word)):
+            word1 = [i:j+1]
 
-for i in range(len(word)):
-    for j in range(i, len(word)):
-        word1 = [i:j+1]
-
-        if word1 == word1[::-1]:
-            l.append(len(word1))
+            if word1 == word1[::-1]:
+                l.append(len(word1))
             
-print(sorted(word)[len(l)-1])
+    print(sorted(word)[len(l)-1])
+    
+hidden_palindrome()
