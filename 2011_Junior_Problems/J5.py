24d956bf5a1cb1523358eea8dd28792194f1d370
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:33:09 -0400

    Update J5.py

--- 2011_Junior_Problems/J5.py
+++ 2011_Junior_Problems/J5.py
@@ -1,26 +1,28 @@
 # CCC 2011 Problem J5: Unfriend
 # Programmer: Foram Gandhi
 
+def unfriend():
+    num = int(input())
+    d = []
 
-num = int(input())
-d = []
+    for i in range(num-1):
+        d.append(input())
 
-for i in range(num-1):
-    d.append(input())
+    def network(n):
+        list = []
+        for i in range(len(d)):
+            if d[i] == str(n):
+                list.append(i+1)
+        return list
 
-def network(n):
-    list = []
-    for i in range(len(d)):
-        if d[i] == str(n):
-            list.append(i+1)
-    return list
+    def invites(node, total):
+        if network(node) == []:
+            return 2
+        for i in (network(node)):
+            total = (total*invites(i, 1))
 
-def invites(node, total):
-    if network(node) == []:
-        return 2
-    for i in (network(node)):
-        total = (total*invites(i, 1))
+        return total + 1
 
-    return total + 1
+    print(invites(num, 1)-1)
 
-print(invites(num, 1)-1)
+unfriend()
