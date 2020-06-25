d0b49793ef93603a339f1fe9a275a1bc78a42da8
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:46:36 -0400

    Update J2.py

--- 2015_Junior_Problems/J2.py
+++ 2015_Junior_Problems/J2.py
@@ -1,15 +1,16 @@
 # CCC 2015 Problem J2: Happy or Sad
 # Programmer: Foram Gandhi
 
+def happy_or_sad():
+    message = str(input())
 
-message = str(input())
+    if message.count(':-)') == 0 or message.count(':-(') == 0:
+        print("none")
+    elif message.count(':-)') == message.count(':-('):
+        print("unsure")
+    elif message.count(':-)') > message.count(':-('):
+        print("happy")
+    elif message.count(':-)') < message.count(':-('):
+        print("sad")
 
-if message.count(':-)') == 0 or message.count(':-(') == 0:
-    print("none")
-elif message.count(':-)') == message.count(':-('):
-    print("unsure")
-elif message.count(':-)') > message.count(':-('):
-    print("happy")
-elif message.count(':-)') < message.count(':-('):
-    print("sad")
-
+happy_or_sad()
