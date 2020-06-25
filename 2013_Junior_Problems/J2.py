14eb96d13d3727a1c1ec27dfad61c6d3788f7eb8
Foram <Foramgandhi12@yahoo.ca>
2020-06-25 17:37:19 -0400

    Update J2.py

--- 2013_Junior_Problems/J2.py
+++ 2013_Junior_Problems/J2.py
@@ -1,17 +1,19 @@
 # CCC 2013 Problem J2: Rotating letters
 # Programmer: Foram Gandhi
 
+def rotating_letters():
+    word = input()
+    rotating_letters = "IOSHZXN"
 
-word = input()
-rotating_letters = "IOSHZXN"
+    count = 0
 
-count = 0
+    for letter in word:
+        if letter in rotating_letters:
+            count += 1
 
-for letter in word:
-    if letter in rotating_letters:
-        count += 1
-
-if count == len(word):
-    print("YES")
-else:
-    print("NO")
+    if count == len(word):
+        print("YES")
+    else:
+        print("NO")
+        
+rotating_letters()
